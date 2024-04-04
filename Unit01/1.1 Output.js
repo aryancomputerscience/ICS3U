fb2cccb4f7b6f7f1044dffd48b528c27bb20f757
aryancomputerscience <159183643+aryancomputerscience@users.noreply.github.com>
2024-04-04 14:07:02 -0400

    Update 1.1 Output.js

--- Unit01/1.1 Output.js
+++ Unit01/1.1 Output.js
@@ -1,6 +1,27 @@
 //1
 
-console.log("Apple Inc.\nOne Apple Park Way \nCupertino, CA \n95014 \n+1 408-996-1010"");
+console.log("Apple Inc.\nOne Apple Park Way \nCupertino, CA \n95014 \n+1 408-996-1010");
+
+
+//2
+
+console.log();
+console.log();
+
+//3
+
+console.log("Microsoft Corporation \nOne Microsoft Way \nRedmond, WA \n98052 \n+1 425-882-8080");
+
+//4
+
+console.log();
+console.log();
+
+//5
+
+console.log("x o x\no x o \nx o x");
+
+
 
 
 
