79233bd6a1ac005d6fe88cd5d85cacdde10da285
aryancomputerscience <159183643+aryancomputerscience@users.noreply.github.com>
2024-04-05 13:54:23 -0400

    Update 1.3 Mathematics.js

--- Unit01/1.3 Mathematics.js
+++ Unit01/1.3 Mathematics.js
@@ -1,1 +1,31 @@
+//part 1
 
+let celsius = 100;
+let fahrenheit = 0;
+celsius = celsius + 2;
+fahrenheit = (celsius * 9/5) + 32;
+console.log(fahrenheit);
+
+
+//part 2
+
+fahrenheit = 77;
+celsius = (fahrenheit - 32) * 5/9;
+console.log(celsius);
+
+//part 3
+
+fahrenheit = 77;
+celsius = (fahrenheit - 32) * 5/9;
+console.log("77 degrees fahrenheit is", celsius, "degrees celsius.");
+
+//slope 
+
+x1 = -4;
+y1 = 5;
+x2 = 6;
+y2 = 0;
+
+m = (y2 - y1)/(x2 - x1);
+
+console.log(m);
